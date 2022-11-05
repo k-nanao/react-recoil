511e417ab466e39a129511e41aa57b4998ce619b
zyyx-k-nanao <k-nanao@zyyx.jp>
2022-11-05 22:52:15 +0900

    emotionが効いたので

--- src/components/TodoListStatus.tsx
+++ src/components/TodoListStatus.tsx
@@ -1,6 +1,8 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react';
 import { useRecoilValue } from 'recoil';
 import { todoListStatusState } from '../State/selector';
+import { css } from '@emotion/react'
 
 const TodoListStatus = () => {
   const { totalNum, totalCompletedNum, totalUnCompletedNum } =
@@ -9,12 +11,16 @@ const TodoListStatus = () => {
   return (
     <div>
       <ul>
-        <li>Todoの登録数: {totalNum}</li>
+        <li css={hellostyle}>Todoの登録数: {totalNum}</li>
         <li>完了数: {totalCompletedNum}</li>
-        <li>未完了数: {totalUnCompletedNum}</li>
+        <li css={hellostyle}>未完了数: {totalUnCompletedNum}</li>
       </ul>
     </div>
   );
 };
+
+const hellostyle = css({
+  color:'red'
+})
 
 export default TodoListStatus;
